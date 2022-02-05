da57422eed82a7b54d87eacbfd29fc4d09991256
ismail hakkı sonalcan <ismail.hakki.sonalcan@gmail.com>
2022-02-05 21:57:57 +0300

    comitted created files

---
+++ MVC_GIT_REPO/MVC_GIT_REPO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_GIT_REPO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
